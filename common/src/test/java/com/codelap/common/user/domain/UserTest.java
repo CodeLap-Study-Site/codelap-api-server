7b05e61562a9bf0201280e2af79232d9bd4ff0d1
audghks1996 <audghks1996@naver.com>
2023-03-10 18:09:19 +0900

    COD-8 유저 도메인 수정 로직 개발

--- common/src/test/java/com/codelap/common/user/domain/UserTest.java
+++ common/src/test/java/com/codelap/common/user/domain/UserTest.java
@@ -1,14 +1,24 @@
 package com.codelap.common.user.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
+import static com.codelap.common.user.domain.User.MIN_AGE;
 import static com.codelap.common.user.domain.UserStatus.CREATED;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static com.codelap.common.user.domain.UserStatus.DELETED;
+import static org.assertj.core.api.Assertions.*;
 
 class UserTest {
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        UserCareer career = UserCareer.create("직무", 1);
+        user = User.create("name", 10, career);
+    }
 
     @Test
     void 유저_생성_성공() {
@@ -35,11 +45,59 @@ class UserTest {
     void 유저_생성_실패__나이가_최소값_보다_작음() {
         UserCareer career = UserCareer.create("직무", 1);
 
-        assertThatIllegalArgumentException().isThrownBy(() -> User.create("name", User.MIN_AGE - 1, career));
+        assertThatIllegalArgumentException().isThrownBy(() -> User.create("name", MIN_AGE - 1, career));
     }
 
     @Test
     void 유저_생성_실패__직무가_널() {
         assertThatIllegalArgumentException().isThrownBy(() -> User.create("name", 10, null));
+    }
+
+    @Test
+    void 유저_수정_성공() {
+        UserCareer updateCareer = UserCareer.create("업데이트된 직무", 2);
+
+        user.update("updateName", 11, updateCareer);
+
+        assertThat(user.getName()).isEqualTo("updateName");
+        assertThat(user.getAge()).isEqualTo(11);
+        assertThat(user.getCareer()).isSameAs(updateCareer);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 유저_수정_실패__이름이_널이거나_공백(String updateName) {
+        UserCareer updateCareer = UserCareer.create("업데이트된 직무", 2);
+
+        assertThatIllegalArgumentException().isThrownBy(() ->
+                user.update(updateName, 11, updateCareer)
+        );
+    }
+
+    @Test
+    void 유저_수정_실패__나이가_최소값_미만() {
+        UserCareer updateCareer = UserCareer.create("업데이트된 직무", 2);
+
+        assertThatIllegalArgumentException().isThrownBy(() ->
+                user.update("updatedName", MIN_AGE - 1, updateCareer)
+        );
+    }
+
+    @Test
+    void 유저_수정_실패__직무가_널() {
+        assertThatIllegalArgumentException().isThrownBy(() ->
+                user.update("updatedName", 11, null)
+        );
+    }
+
+    @Test
+    void 유저_수정_실패__생성됨_상태가_아님() {
+        user.setStatus(DELETED);
+
+        UserCareer updateCareer = UserCareer.create("업데이트된 직무", 2);
+
+        assertThatIllegalStateException().isThrownBy(() ->
+                user.update("updatedName", 11, updateCareer)
+        );
     }
 }
