bb9b0a6fbb4678cb9392b006d7e55a0183d4ed11
Kawkmyunghwan <audghks1996@naver.com>
2023-03-14 11:26:18 +0900

    COD-32 스터디 참가신청 도메인 서비스 생성 로직

---
+++ common/src/main/java/com/codelap/common/studyRequest/domain/StudyRequestRepository.java
@@ -0,0 +1,6 @@
+package com.codelap.common.studyRequest.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudyRequestRepository extends JpaRepository<StudyRequest, Long> {
+}
---
+++ common/src/test/java/com/codelap/common/studyRequest/service/StudyRequestDomainServiceTest.java
@@ -0,0 +1,65 @@
+package com.codelap.common.studyRequest.service;
+
+import com.codelap.common.study.domain.Study;
+import com.codelap.common.study.domain.StudyNeedCareer;
+import com.codelap.common.study.domain.StudyPeriod;
+import com.codelap.common.study.domain.StudyRepository;
+import com.codelap.common.studyRequest.domain.StudyRequest;
+import com.codelap.common.studyRequest.domain.StudyRequestRepository;
+import com.codelap.common.studyRequest.domain.StudyRequestStatus;
+import com.codelap.common.user.domain.User;
+import com.codelap.common.user.domain.UserCareer;
+import com.codelap.common.user.domain.UserRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.OffsetDateTime;
+
+import static com.codelap.common.study.domain.StudyDifficulty.NORMAL;
+import static com.codelap.common.studyRequest.domain.StudyRequestStatus.*;
+import static org.assertj.core.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+class StudyRequestDomainServiceTest {
+
+    @Autowired
+    private StudyRequestService studyRequestService;
+
+    @Autowired
+    private StudyRepository studyRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    StudyRequestRepository studyRequestRepository;
+
+    private Study study;
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        UserCareer career = UserCareer.create("직무", 1);
+        User leader = userRepository.save(User.create("name", 10, career, "abcd", "setup"));
+
+        StudyPeriod period = StudyPeriod.create(OffsetDateTime.now(), OffsetDateTime.now().plusMinutes(10));
+        StudyNeedCareer needCareer = StudyNeedCareer.create("직무", 1);
+
+        study = studyRepository.save(Study.create("팀", "설명", 4, NORMAL, period, needCareer, leader));
+
+        user = userRepository.save(User.create("user", 10, career, "abcd", "email"));
+    }
+
+    @Test
+    void 스터디_참가_신청_성공() {
+        studyRequestService.create(user.getId(), study.getId(), "참가신청");
+
+        StudyRequest studyRequest = studyRequestRepository.findAll().get(0);
+
+        assertThat(studyRequest.getId()).isNotNull();
+    }
+}
