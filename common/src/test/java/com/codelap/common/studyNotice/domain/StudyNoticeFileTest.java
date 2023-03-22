85efded64feb4cecfadb39fac6ae95349453d3f4
audghks1996 <audghks1996@naver.com>
2023-03-22 22:44:11 +0900

    COD-77 스터디 공지 도메인 공지파일 테스트코드 작성

---
+++ common/src/test/java/com/codelap/common/studyNotice/domain/StudyNoticeFileTest.java
@@ -0,0 +1,39 @@
+package com.codelap.common.studyNotice.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class StudyNoticeFileTest {
+    private static final Long MIN_SIZE = 1L;
+
+    @Test
+    void 스터디_인증_파일_생성_성공() {
+        StudyNoticeFile studyNoticeFile = StudyNoticeFile.create("savedName", "originalName", 100L);
+
+        assertThat(studyNoticeFile.getSavedName()).isEqualTo("savedName");
+        assertThat(studyNoticeFile.getOriginalName()).isEqualTo("originalName");
+        assertThat(studyNoticeFile.getSize()).isEqualTo(100L);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 스터디_인증_생성_실패__저장명이_공백_혹은_널(String savedName) {
+        assertThatIllegalArgumentException().isThrownBy(() -> StudyNoticeFile.create(savedName, "originalName", 100L));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 스터디_인증_생성_실패__원본명이_공백_혹은_널(String originalName) {
+        assertThatIllegalArgumentException().isThrownBy(() -> StudyNoticeFile.create("savedName", originalName, 100L));
+    }
+
+    @Test
+    void 스터디_인증_생성_실패__원본명이_공백_혹은_널() {
+        assertThatIllegalArgumentException().isThrownBy(() -> StudyNoticeFile.create("savedName", "originalName", MIN_SIZE - 1));
+    }
+
+}
