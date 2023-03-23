0539fcab8d2f20160760944412c267a83f4760e8
audghks1996 <audghks1996@naver.com>
2023-03-23 22:27:38 +0900

    COD-78 스터디 공지 도메인 서비스 설계

---
+++ common/src/main/java/com/codelap/common/studyNotice/domain/StudyNoticeRepository.java
@@ -0,0 +1,6 @@
+package com.codelap.common.studyNotice.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudyNoticeRepository extends JpaRepository<StudyNotice, Long> {
+}
---
+++ common/src/test/java/com/codelap/common/studyNotice/service/StudyNoticeDomainServiceTest.java
@@ -0,0 +1,77 @@
+package com.codelap.common.studyNotice.service;
+
+import com.codelap.common.study.domain.Study;
+import com.codelap.common.study.domain.StudyNeedCareer;
+import com.codelap.common.study.domain.StudyPeriod;
+import com.codelap.common.study.domain.StudyRepository;
+import com.codelap.common.studyNotice.domain.StudyNotice;
+import com.codelap.common.studyNotice.domain.StudyNoticeFile;
+import com.codelap.common.studyNotice.domain.StudyNoticeRepository;
+import com.codelap.common.user.domain.User;
+import com.codelap.common.user.domain.UserCareer;
+import com.codelap.common.user.domain.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+import static com.codelap.common.study.domain.StudyDifficulty.NORMAL;
+import static com.codelap.common.support.CodeLapExceptionTest.assertThatActorValidateCodeLapException;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+@SpringBootTest
+class StudyNoticeDomainServiceTest {
+
+    @Autowired
+    private StudyNoticeService studyNoticeService;
+    
+    @Autowired
+    private StudyRepository studyRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private StudyNoticeRepository studyNoticeRepository;
+    
+    private User leader;
+    private Study study;
+    private StudyNoticeFile file;
+
+    @BeforeEach
+    void setUp() {
+        UserCareer career = UserCareer.create("직무", 1);
+        leader = userRepository.save(User.create("name", 10, career, "abcd", "setup"));
+
+        StudyPeriod period = StudyPeriod.create(OffsetDateTime.now(), OffsetDateTime.now().plusMinutes(10));
+        StudyNeedCareer needCareer = StudyNeedCareer.create("직무", 1);
+        study = studyRepository.save(Study.create("팀", "설명", 4, NORMAL, period, needCareer, leader));
+
+        file = StudyNoticeFile.create("saved", "original", 10L);
+    }
+
+    @Test
+    void 스터디_공지_생성_성공() {
+        studyNoticeService.create(study.getId(), leader.getId(), "title", "contents", List.of(file));
+
+        StudyNotice studyNotice = studyNoticeRepository.findAll().get(0);
+
+        assertThat(studyNotice.getId()).isNotNull();
+        assertThat(studyNotice.getTitle()).isEqualTo("title");
+        assertThat(studyNotice.getContents()).isEqualTo("contents");
+        assertThat(studyNotice.getFiles()).isNotNull();
+    }
+
+    @Test
+    void 스터디_공지_생성_실패__리더가_아님() {
+        UserCareer career = UserCareer.create("직무", 1);
+        User fakeLeader = userRepository.save(User.create("fakeLeader", 10, career, "abcd", "fakeLeader"));
+
+        assertThatActorValidateCodeLapException().isThrownBy(() -> studyNoticeService.create(study.getId(), fakeLeader.getId(), "title", "contents", List.of(file)));
+    }
+}
