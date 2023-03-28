d5cb58561a815a9541a1468caac803379932acd4
audghks1996 <audghks1996@naver.com>
2023-03-28 11:58:12 +0900

    COD-99 플라이웨이 sql파일 추가 및 수정

---
+++ api/src/main/resources/db/migration/V1_0_7__add_study_notice_comment.sql
@@ -0,0 +1,21 @@
+create table study_notice_comment
+(
+    id              bigint not null,
+    content         varchar(255),
+    create_at       datetime(6),
+    status          varchar(255),
+    study_notice_id bigint,
+    user_id         bigint,
+    primary key (id)
+);
+
+create table study_notice_comment_seq
+(
+    next_val bigint
+);
+
+alter table study_notice_comment
+    add constraint FKm41mm0asxh70w7dweqix17q15 foreign key (study_notice_id) references study_notice (id);
+
+alter table study_notice_comment
+    add constraint FKmdhjsk7noqaf3negppex5387b foreign key (user_id) references user (id);
