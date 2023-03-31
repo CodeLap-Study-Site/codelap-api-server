ca1ecf921db62183baf8efc1a9ee540e1932b975
audghks1996 <audghks1996@naver.com>
2023-03-31 12:23:52 +0900

    COD-102 스터디 댓글 도메인 설계

---
+++ common/src/main/java/com/codelap/common/studyComment/domain/StudyComment.java
@@ -0,0 +1,55 @@
+package com.codelap.common.studyComment.domain;
+
+import com.codelap.common.study.domain.Study;
+import com.codelap.common.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+
+import static com.codelap.common.studyComment.domain.StudyCommentStatus.CREATED;
+import static com.codelap.common.support.Preconditions.require;
+import static jakarta.persistence.EnumType.STRING;
+import static java.util.Objects.nonNull;
+import static org.apache.logging.log4j.util.Strings.isNotBlank;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StudyComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Study study;
+
+    @ManyToOne
+    private User user;
+
+    private String comment;
+
+    private final OffsetDateTime createAt = OffsetDateTime.now();
+
+    @Enumerated(STRING)
+    private final StudyCommentStatus status = CREATED;
+
+    public boolean containsMember(User user) {
+        return study.getMembers().contains(user);
+    }
+
+    private StudyComment(User user, String comment) {
+        this.user = user;
+        this.comment = comment;
+    }
+
+    public static StudyComment create(Study study, User user, String comment) {
+        require(nonNull(study));
+        require(nonNull(user));
+        require(isNotBlank(comment));
+
+        return new StudyComment(user, comment);
+    }
+}
