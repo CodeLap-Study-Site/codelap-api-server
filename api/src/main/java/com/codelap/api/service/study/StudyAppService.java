a8cd19b2058ddd34a806cb4f00e9dcc4f23102f5
audghks1996 <audghks1996@naver.com>
2023-04-11 21:58:51 +0900

    COD-118 본인이 참여한 스터디 조회 로직 설계

--- api/src/main/java/com/codelap/api/service/study/StudyAppService.java
+++ api/src/main/java/com/codelap/api/service/study/StudyAppService.java
@@ -1,10 +1,14 @@
 package com.codelap.api.service.study;
 
 import com.codelap.api.service.study.dto.GetStudiesDto.GetStudiesStudyDto;
+import com.codelap.common.study.dto.GetMyStudiesDto;
+import com.codelap.common.user.domain.User;
 
 import java.util.List;
 
 public interface StudyAppService {
 
     List<GetStudiesStudyDto> getStudies(Long userId);
+
+    List<GetMyStudiesDto> getAllStudies(User user);
 }
