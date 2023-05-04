c940723c23cbbaa5e35e5cc2f61c195e1e9d5cab
Jun, Ahah <96584832+peubel@users.noreply.github.com>
2023-05-04 14:52:36 +0900

    COD-190 유저 활성화 상태 확인 서비스 개발

--- api/src/test/java/com/codelap/api/service/user/DefaultUserAppServiceTest.java
+++ api/src/test/java/com/codelap/api/service/user/DefaultUserAppServiceTest.java
@@ -1,13 +1,14 @@
 package com.codelap.api.service.user;
 
+import com.codelap.common.user.domain.User;
 import com.codelap.common.user.domain.UserRepository;
 import jakarta.transaction.Transactional;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static com.codelap.fixture.UserFixture.createActivateUser;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -23,7 +24,16 @@ public class DefaultUserAppServiceTest {
     void 닉네임_중복_체크_성공_닉네임_있음() {
         userRepository.save(createActivateUser("member"));
 
-        Assertions.assertThat(userAppService.getDuplicateCheckByName("member")).isTrue();
-        Assertions.assertThat(userAppService.getDuplicateCheckByName("fakeName")).isFalse();
+        assertThat(userAppService.getDuplicateCheckByName("member")).isTrue();
+        assertThat(userAppService.getDuplicateCheckByName("fakeName")).isFalse();
+    }
+
+    @Test
+    void 유저_활성화_상태_체크() {
+        User user = userRepository.save(createActivateUser("member"));
+
+        boolean result = userAppService.isActivated(user.getId());
+
+        assertThat(result).isTrue();
     }
 }
