e07537418bb9f0028d6eb43384fd1deabb706bca
Jun, Ahah <96584832+peubel@users.noreply.github.com>
2023-05-04 15:02:01 +0900

    COD-190 유저 활성화 상태 확인 api 개발

--- api/src/main/java/com/codelap/api/controller/user/UserController.java
+++ api/src/main/java/com/codelap/api/controller/user/UserController.java
@@ -2,6 +2,7 @@ package com.codelap.api.controller.user;
 
 import com.codelap.api.controller.user.dto.UserUpdateDto.UserUpdateRequest;
 import com.codelap.api.security.user.DefaultCodeLapUser;
+import com.codelap.api.service.user.UserAppService;
 import com.codelap.common.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+    private final UserAppService userAppService;
 
     @PostMapping("/activate")
     public void activate(
@@ -36,5 +38,12 @@ public class UserController {
             @AuthenticationPrincipal DefaultCodeLapUser user
     ) {
         userService.delete(user.getId());
+    }
+
+    @GetMapping("/is-activated")
+    public boolean isActivated(
+            @AuthenticationPrincipal DefaultCodeLapUser user
+    ) {
+        return userAppService.isActivated(user.getId());
     }
 }
