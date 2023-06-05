af9c4caddcf75576fcf1cd05475216ed945c4a83
yookyungmin <ykmykm4608@naver.com>
2023-06-05 17:01:31 +0900

    COD-126 Fix : 컨플릭트 수정, 전체 테스트 문제 없음.

--- api/src/main/java/com/codelap/api/service/study/DefaultStudyQueryAppService.java
+++ api/src/main/java/com/codelap/api/service/study/DefaultStudyQueryAppService.java
@@ -41,35 +41,7 @@ public class DefaultStudyQueryAppService extends DynamicCond implements StudyQue
 
     @Override
     public List<GetStudyInfo> findStudyCardsByCond(User user, String statusCond, List<TechStack> techStackList) {
-        return queryFactory
-                .selectDistinct(
-                        constructor(
-                                GetStudyInfo.class,
-                                QStudy.study.id,
-                                QStudy.study.name,
-                                QStudy.study.period,
-                                QStudy.study.leader.name,
-                                ExpressionUtils.as(JPAExpressions
-                                                .select(count(QStudyComment.studyComment.id))
-                                                .from(QStudyComment.studyComment)
-                                                .where(QStudyComment.studyComment.study.id.eq(QStudy.study.id)),
-                                        "commentCount"
-                                ),
-                                ExpressionUtils.as(JPAExpressions
-                                                .select(count(QStudyView.studyView.id))
-                                                .from(QStudyView.studyView)
-                                                .where(QStudyView.studyView.study.id.eq(QStudy.study.id)),
-                                        "viewCount"
-                                ),
-                                ExpressionUtils.as(JPAExpressions
-                                                .select(count(QBookmark.bookmark.id))
-                                                .from(QBookmark.bookmark)
-                                                .where(QBookmark.bookmark.study.id.eq(QStudy.study.id)),
-                                        "bookmarkCount"
-                                ),
-                                QStudy.study.maxMembersSize))
-                .from(QStudy.study)
-                .innerJoin(QStudy.study.techStackList, QStudyTechStack.studyTechStack)
+        return getGetStudyInfoJPAQuery()
                 .where(QStudy.study.members.contains(user))
                 .where(QStudy.study.status.ne(DELETED))
                 .where(checkStatus(statusCond))
